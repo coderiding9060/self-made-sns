c1a8a4d175a5b858f3eba4f15ea4f95421c2c856
HOME <coderiding9060@gmail.com>
2021-09-30 21:02:47 +0900

    Initial Commit

---
+++ src/main/java/self/selfmadesns/SelfMadeSnsApplication.java
@@ -0,0 +1,13 @@
+package self.selfmadesns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfMadeSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfMadeSnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/self/selfmadesns/SelfMadeSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package self.selfmadesns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfMadeSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
