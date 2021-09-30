231034b5a2c55e951e098893c3dee0427de909f6
HOME <coderiding9060@gmail.com>
2021-10-01 01:32:24 +0900

    thymeleaf 레이아웃으로 메인페이지, 회원가입페이지 html 작성 및 .js/.css파일 추가 회원가입기능 구현(아이디중복/이메일중복확인은 미구현)

--- src/main/java/self/selfmadesns/members/controller/RestMemberController.java
+++ src/main/java/self/selfmadesns/members/controller/RestMemberController.java
@@ -1,10 +1,41 @@
 package self.selfmadesns.members.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import self.selfmadesns.members.domain.Member;
+import self.selfmadesns.members.service.MemberService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @Transactional
 public class RestMemberController {
+
+    private MemberService memberService;
+
+    @Autowired
+    public RestMemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping(value = "/members", headers = {"Content-type=application/json;charset=utf-8"})
+    @ResponseBody
+    public Map<String, Object> join(@RequestBody(required = false) Member member) {
+        Map result = new HashMap();
+        result.put("result", Boolean.FALSE);
+        if(member!=null){
+            Member insertResult = memberService.createMember(member);
+            if(insertResult!=null){
+                System.out.println("insertResult = " + insertResult);
+                result.put("result", Boolean.TRUE);
+            }
+        }
+        return result;
+    }
 
 }
